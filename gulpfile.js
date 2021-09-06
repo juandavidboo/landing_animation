b49c7d69609aa933903f14d80666e5fab39a45b7
jdboo <juandavidboo@users.noreply.github.com>
2021-09-06 11:20:26 -0300

    frontend: setup and html structure

--- gulpfile.js
+++ gulpfile.js
@@ -3,62 +3,63 @@ const
 	del = require('delete'),
 	sass = require('gulp-sass'),
 	browserSync = require('browser-sync').create(),
-	bs_reload = browserSync.reload
-
+	bSync_reload = browserSync.reload
 
 let dist = 'dist/';
 let src = 'src/'
 
+function clean_folder (cb) {
+	del([dist + '/*'], cb);
+}
 
-// CLEAN Working Folder
-
-gulp.task('clean_folder', function(cb) {
-	del([dist + '*'], cb);
-});
-gulp.task('clean:dist', function() {
-	return del.sync(dist + '*');
-});
-
-// copy and compile
-
-gulp.task('html', function() {
+function html() {
 	return gulp.src(src + '*.*')
 	.pipe(gulp.dest(dist))
-});
+}
 
-gulp.task('css', function() {
-	return gulp.src([src + 'css/**/*.css'])
-	.pipe(gulp.dest(dist + 'css'))
-});
+function fonts(){
+	return gulp.src(src + 'fonts/**/*.*')
+	.pipe(gulp.dest(dist + '/fonts'));
+}
 
-gulp.task('images', function() {
-	return gulp.src([src + 'img/**/*.*'])
-	.pipe(gulp.dest(dist + 'img'))
-});
+function css(){
+	return gulp.src(src + 'css/**/*.css')
+	.pipe(gulp.dest(dist + '/css'));
+}
 
-gulp.task('sass', function() {
-	return gulp.src([src + 'sass/**/*.+(scss|sass)'])
+function sass_css(){
+	return gulp.src(src + 'sass/**/*.+(scss|sass)')
 	.pipe(sass().on('error', sass.logError))
-	.pipe(gulp.dest(dist + 'css'))
-});
+	.pipe(gulp.dest(dist + '/css'));
+}
 
-gulp.task('scripts', function() {
-	return gulp.src([src + 'js/**/*.js'])
-	.pipe(gulp.dest(dist + 'js'))
-});
 
-gulp.task('browserSync', function() {
-	browserSync.init({
-	  server: {
+function scripts(){
+	return gulp.src(src + 'js/**/*.js')
+	.pipe(gulp.dest(dist + '/js'));
+}
+
+function img(){
+	return gulp.src(src + 'img/**/*.*')
+	.pipe(gulp.dest(dist + '/img'));
+}
+
+// Serve to browser
+function serve(done) {
+	browserSync.init(null, {
+		notify: false,
+		server: {
 		baseDir: dist
-	  },
-	})
-})
+		}
+	});
+	done();
+}
 
+// Watch for changes
+function watch_src() {
+	gulp.watch(src, gulp.series(html, css, sass_css, img, scripts))
+		.on('change', bSync_reload);
+}
 
-gulp.task('watch_me', function(){
-	//gulp.watch('app/scss/**/*.scss', ['sass']); 
-	// Other watchers
-	gulp.watch(src, gulp.series(['clean_folder','html','css','img','sass','js'])
-	.on('change', bs_reload));
-  })
+exports.default = gulp.series(clean_folder, fonts, html, css, sass_css, img, scripts, serve, watch_src);
+exports.build = gulp.series(clean_folder, fonts, html, css, sass_css, img, scripts);
