060c7fc7c1aac200114d9051eddc4edeab3c3d6f
jdboo <juandavidboo@users.noreply.github.com>
2021-09-13 15:39:57 -0300

    frontend: styling various

--- src/js/plugins.js
+++ src/js/plugins.js
@@ -28,26 +28,51 @@ AOS.init({
   throttleDelay: 99
 });
 
-window.addEventListener("load", function(){
-
-  var tl = new gsap.timeline({onComplete:function(){tl.play("start")}});
-
-  tl.defaultEase = Power1.easeInOut;
-  tl.delay(0.5)
-    .add("start")
-    .to(".ptext_01", 0.5, {top:'50%'}, "-=0.5")
-    .to(".ptext_01", 0.5, {top:'-150%'}, "+=1")
-    .to(".ptext_02", 0.5, {top:'50%'}, "-=0.5")
-    .to(".ptext_02", 0.5, {top:'-150%'}, "+=1")
-    .to(".ptext_03", 0.5, {top:'50%'}, "-=0.5")
-    .to(".ptext_03", 0.5, {top:'-150%'}, "+=1")
-    .to(".ptext_04", 0.5, {top:'50%'}, "-=0.5")
-    .to(".ptext_04", 0.5, {top:'-150%'}, "+=1")
-    .to(".ptext_05", 0.5, {top:'50%'}, "-=0.5")
-    .to(".ptext_05", 0.5, {top:'-150%'}, "+=1")
-    .to(".ptext_06", 0.5, {top:'50%'}, "-=0.5")
-    .to(".ptext_06", 0.5, {top:'-150%'}, "+=1")
+// wait until DOM is ready
+document.addEventListener("DOMContentLoaded", function(event) {
+  gsap.registerPlugin(CSSRulePlugin, ScrollToPlugin, ScrollTrigger);
   
- });
+  // wait until external stylesheets, links, images, and other external assets are loaded in the window
+  window.addEventListener("load", function(){
+
+    function intro() {
+      var tl = new gsap.timeline({onComplete:function(){tl.play("start")}});
+  
+      var rule = CSSRulePlugin.getRule(".pill::before"); //get the rule
+      //gsap.to(rule, {duration: 3, cssRule: {color: "blue"}});
+    
+      tl.defaultEase = Power1.easeInOut;
+      tl.delay(0)
+        .to(rule, {duration: 0, cssRule: {opacity: 1}})
+        .to(rule, {duration: 0.5, cssRule: {width: "120%"}})
+        .add("start")
+        //.to(".pill_text_01", 0, {top:'150%'})
+        .to(".pill_text_01", 0.5, {top:'50%'}, "-=0.5")
+        .to(".pill_text_01", 0.5, {top:'-150%'}, "+=1")
+        .to(".pill_text_02", 0.5, {top:'50%'}, "-=0.5")
+        .to(".pill_text_02", 0.5, {top:'-150%'}, "+=1")
+        .to(".pill_text_03", 0.5, {top:'50%'}, "-=0.5")
+        .to(".pill_text_03", 0.5, {top:'-150%'}, "+=1")
+        .to(".pill_text_04", 0.5, {top:'50%'}, "-=0.5")
+        .to(".pill_text_04", 0.5, {top:'-150%'}, "+=1")
+        .to(".pill_text_05", 0.5, {top:'50%'}, "-=0.5")
+        .to(".pill_text_05", 0.5, {top:'-150%'}, "+=1")
+        .to(".pill_text_06", 0.5, {top:'50%'}, "-=0.5")
+      
+      return tl;
+    }
+
+    var master = gsap.timeline();
+    master.add(intro())
+          //.add(middle(), "+=2")     //with a gap of 2 seconds
+          
+    
+    
+  }, false);
+  
+});
+
+
+
 
 
